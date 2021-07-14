20c2872572538e578f6f0cfe65c4211d44c1b827
jonathan52 <dangkhoahathuc@gmail.com>
2021-07-14 19:21:21 +0700

    Befort refactoring

---
+++ src/main/java/rookies/demo/controller/AuthController.java
@@ -0,0 +1,15 @@
+package rookies.demo.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/auth")
+public class AuthController {
+    
+    @PostMapping("/login")
+    public String login(){
+        return "Logged in";
+    }
+}
